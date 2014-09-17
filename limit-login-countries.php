4586a79a039a291ae196b1e9965c2bcd3fe2af8c
wedi <code@dirk-weise.de>
2014-09-17 16:00:31 +0200

    Refactoring. Move plugin public functionality to own class.

--- limit-login-countries.php
+++ limit-login-countries.php
@@ -12,6 +12,7 @@ Author URI: http://dirk-weise.de/
 License: GPLv2 or later
 */
 
+/** Run the plugin. */
 Limit_Login_Countries::get_instance();
 
 /**
@@ -22,14 +23,12 @@ Limit_Login_Countries::get_instance();
  */
 class Limit_Login_Countries {
 
-	/** @var $instance Limit_Login_Countries Stores the single plugin object instance. */
+	/** @var $instance LLC_Public Stores the single plugin object instance. */
 	protected static $instance;
 
-	/** @var $options Array Stores the plugin's options. */
-	protected $options = array();
+	/** @var $instance Limit_Login_Countries Stores the single plugin object instance. */
+	protected static $slug = 'limit-login-countries';
 
-	/** @var $geoInfo geoiprecord Stores the visitor's geo information. */
-	protected $geoInfo;
 
 	/**
 	 * Constructor of plugin class.
@@ -40,18 +39,24 @@ class Limit_Login_Countries {
 	 */
 	protected function __construct() {
 
-		// we use the activation hook to run some code on plugin installation
+		// The code that runs during plugin activation.
+		require_once __DIR__ . '/includes/LLC_Activator.class.php';
 		register_activation_hook( __FILE__, array( 'LLC_Activator', 'activate' ) );
 
-		// we add an init hook for loading options and our textdomain for l10n
-		add_action( 'init', array( $this, 'loadOptions' ) );
+		// load translation
+		add_action( 'plugins_loaded', array( $this, 'load_plugin_textdomain' ) );
 
-		// We add the authentication filter
-		add_filter( 'wp_authenticate_user', array( $this, 'limit_login_countries' ), 31, 1 );
+		// load the public functionality of the plugin
+		require_once( __DIR__ . '/public/LLC_Public.class.php' );
+		LLC_Public::get_instance();
 
-		// we set up everything needed for our settings page
-		require_once( __DIR__ . '/includes/LLC_Options_Page.class.php' );
-		LLC_OPTIONS_PAGE::init();
+		// load the admin functionality of the plugin
+		if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+			require_once( __DIR__ . '/includes/LLC_Options_Page.class.php' );
+			//require_once( __DIR__ . '/admin/LLC_Options_Page.class.php' );
+			LLC_Options_Page::init();
+		}
+
 	}
 
 	/**
@@ -70,89 +75,15 @@ class Limit_Login_Countries {
 	}
 
 	/**
-	 * Loads the plugin's options
+	 * Load the plugin text domain for translation.
 	 *
-	 * @since 0.2
-	 *
-	 * @return void
+	 * @since 0.7
 	 */
-	public function loadOptions() {
-
-		$this->options['plugindir'] = plugin_dir_path( __FILE__ );
-
-		$this->options['geoip_database'] = get_option( 'llc_geoip_database_path' );
-		$this->options['blacklist']      = 'whitelist' === get_option( 'llc_blacklist', 'whitelist' ) ? false : true;
-		$this->options['countryList']    = explode( ',', get_option( 'llc_countries' ) );
-
-		load_plugin_textdomain( 'limit-login-countries', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-
-	}
-
-	/**
-	 * WP_Authenticate_User filter callback function. Here the magic is done: we stop authentication if needed.
-	 * This filter is registred with priority 31. We can prevent login by returning a WP_Error object.
-	 *
-	 * @since 0.1
-	 * @see https://codex.wordpress.org/Plugin_API/Filter_Reference/wp_authenticate_user
-	 *
-	 * @param $user Mixed NULL: no authentication yet. WP_Error: another process has already failed the authentication. WP_User: another process has authenticated the user.
-	 *
-	 * @return Mixed We return WP_Error when the visitor's country is not allowed. In all other cases we pass on whatever we got via $user.
-	 */
-	public function limit_login_countries( $user ) {
-
-		// In these cases we don't throw an error, but pass on what we got:
-		if (
-			is_wp_error( $user )                            // there already is an authentication error
-			or ! $this->geoLookUp()                        // there is no geo info available
-			or empty( $this->options['countryList'] )        // there is no country set in options
-			or $this->isAllowedCountry()                // the user's country is allowed
-			or ( defined( 'LIMIT_LOGIN_COUNTRIES_OVERRIDE' ) and true === LIMIT_LOGIN_COUNTRIES_OVERRIDE )    // override constant is defined
-		) {
-			return $user;
-		}
-
-		// we are still here, so we complain about the user's country
-		// translators: %s stands for the country name the user tries to login from
-		$user = new WP_Error( 'country_error', sprintf( __( 'Login not allowed from your country (%s)!', 'limit-login-countries' ), __( $this->geoInfo->country_name, 'limit-login-countries' ) ) );
-
-		// we save the unsuccessful country code
-		$log = get_option( 'limit_login_countries_log', array() );
-		$log[ $this->geoInfo->country_code ] += 1;
-		update_option( 'limit_login_countries_log', $log );
-
-		return $user;
-	}
-
-	/**
-	 * Look up of visitor's geo information.
-	 * Done seperately from __construct() because we only need it when authenticating.
-	 *
-	 * @since 0.2
-	 *
-	 * @return bool Returns TRUE on success and FALSE if there is no geo information available (e.g. localhost) or an error occurred.
-	 */
-	protected function geoLookUp() {
-
-		// we check whether geo info is already loaded
-		if ( ! is_object( $this->geoInfo ) ) {
-			require_once( __DIR__ . '/includes/LLC_GeoIP_Tools.class.php' );
-			$this->geoInfo = LLC_GeoIP_Tools::getGeoInfo( $this->options['geoip_database'] );
-		}
-
-		// return false if no info was found (e.g. localhost) or there was an error
-		return ( null === $this->geoInfo OR false === $this->geoInfo OR '' === $this->geoInfo->country_code ) ? false : true;
-	}
-
-	/**
-	 * Checks whether visitor is allowed to login from his country.
-	 *
-	 * @since 0.1
-	 *
-	 * @return bool Returns TRUE if visitor's country is allowed to login, FALSE if not.
-	 */
-	protected function isAllowedCountry() {
-
-		return $this->options['blacklist'] xor in_array( $this->geoInfo->country_code, $this->options['countryList'] );
+	public function load_plugin_textdomain() {
+		load_plugin_textdomain(
+			Limit_Login_Countries::$slug,
+			false,
+			dirname( plugin_basename( __FILE__ ) ) . '/languages/'
+		);
 	}
 }
---
+++ public/LLC_Public.class.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Encapsulates the public functionality of the plugin aka the login checking.
+ *
+ * @since 0.7
+ */
+class LLC_Public {
+
+	/** @var $instance LLC_Public Stores the single plugin object instance. */
+	protected static $instance;
+
+	/** @var $options Array Stores the plugin's options. */
+	protected $options = array();
+
+	/** @var $geoInfo geoiprecord Stores the visitor's geo information. */
+	protected $geoInfo;
+
+
+	/**
+	 * Initialize the class, set its properties and register needed WordPress Hooks.
+	 *
+	 * @since 0.7
+	 */
+	protected function __construct() {
+
+		// we add an init hook for loading options
+		add_action( 'init', array( $this, 'loadOptions' ) );
+
+		// We add the authentication filter
+		add_filter( 'wp_authenticate_user', array( $this, 'limit_login_countries' ), 31, 1 );
+
+	}
+
+	/**
+	 * Returns and if necessary creates the plugin object instance
+	 *
+	 * @since 0.7
+	 *
+	 * @return Limit_Login_Countries
+	 */
+	public static function get_instance() {
+		if ( null === self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Loads the plugin's options
+	 *
+	 * @since 0.7
+	 *
+	 * @return void
+	 */
+	public function loadOptions() {
+		$this->options['geoip_database'] = get_option( 'llc_geoip_database_path' );
+		$this->options['blacklist']      = 'whitelist' === get_option( 'llc_blacklist', 'whitelist' ) ? false : true;
+		$this->options['countryList']    = explode( ',', get_option( 'llc_countries' ) );
+	}
+
+	/**
+	 * WP_Authenticate_User filter callback function. Here the magic is done: we stop authentication if needed.
+	 * This filter is registred with priority 31. We can prevent login by returning a WP_Error object.
+	 *
+	 * @since 0.7
+	 * @see https://codex.wordpress.org/Plugin_API/Filter_Reference/wp_authenticate_user
+	 *
+	 * @param $user Mixed NULL: no authentication yet. WP_Error: another process has already failed the authentication. WP_User: another process has authenticated the user.
+	 *
+	 * @return Mixed We return WP_Error when the visitor's country is not allowed. In all other cases we pass on whatever we got via $user.
+	 */
+	public function limit_login_countries( $user ) {
+
+		// In these cases we don't throw an error, but pass on what we got:
+		if (
+			is_wp_error( $user )                            // there already is an authentication error
+			or ! $this->geoLookUp()                        // there is no geo info available
+			or empty( $this->options['countryList'] )        // there is no country set in options
+			or $this->isAllowedCountry()                // the user's country is allowed
+			or ( defined( 'LIMIT_LOGIN_COUNTRIES_OVERRIDE' ) and true === LIMIT_LOGIN_COUNTRIES_OVERRIDE )    // override constant is defined
+		) {
+			return $user;
+		}
+
+		// we are still here, so we complain about the user's country
+		// translators: %s stands for the country name the user tries to login from
+		$user = new WP_Error( 'country_error', sprintf( __( 'Login not allowed from your country (%s)!', 'limit-login-countries' ), __( $this->geoInfo->country_name, 'limit-login-countries' ) ) );
+
+		// we save the unsuccessful country code
+		$log = get_option( 'limit_login_countries_log', array() );
+		$log[ $this->geoInfo->country_code ] += 1;
+		update_option( 'limit_login_countries_log', $log );
+
+		return $user;
+	}
+
+	/**
+	 * Look up of visitor's geo information.
+	 * Done seperately from __construct() because we only need it when authenticating.
+	 *
+	 * @since 0.7
+	 *
+	 * @return bool Returns TRUE on success and FALSE if there is no geo information available (e.g. localhost) or an error occurred.
+	 */
+	protected function geoLookUp() {
+
+		// we check whether geo info is already loaded
+		if ( ! is_object( $this->geoInfo ) ) {
+			require_once( dirname( __DIR__ ) . '/includes/LLC_GeoIP_Tools.class.php' );
+			$this->geoInfo = LLC_GeoIP_Tools::getGeoInfo( $this->options['geoip_database'] );
+		}
+
+		// return false if no info was found (e.g. localhost) or there was an error
+		return ( null === $this->geoInfo OR false === $this->geoInfo OR '' === $this->geoInfo->country_code ) ? false : true;
+	}
+
+	/**
+	 * Checks whether visitor is allowed to login from his country.
+	 *
+	 * @since 0.7
+	 *
+	 * @return bool Returns TRUE if visitor's country is allowed to login, FALSE if not.
+	 */
+	protected function isAllowedCountry() {
+
+		return $this->options['blacklist'] xor in_array( $this->geoInfo->country_code, $this->options['countryList'] );
+	}
+
+}
