4f178b512f3304b94a9d706dcc2cd18893176a15
wedi <code@dirk-weise.de>
2014-09-17 23:38:10 +0200

    Fix JSHint Warnings.

--- admin/js/limit-login-countries.js
+++ admin/js/limit-login-countries.js
@@ -7,46 +7,47 @@
  */
 
 jQuery(document).ready(function ($) {
+    "use strict";
 
-    $('#llc_blacklist').change(function () {
+    $("#llc_blacklist").change(function () {
         // we change the label of country list according to whitelist or blacklist mode
-        $('label[for="llc_countries_js"]').text(llc_countries_label[$(this).val()]);
+        $("label[for='llc_countries_js']").text(llc_countries_label[$(this).val()]);
     });
 
     // we hide the non JS text input and add the TextExt textarea.
-    $('#llc_countries').hide().after("<textarea id='llc_countries_js' name='llc_countries_js' rows='1' cols='52'></textarea>");
+    $("#llc_countries").hide().after("<textarea id='llc_countries_js' name='llc_countries_js' rows='1' cols='52'></textarea>");
 
     // we change the country list label to reference our new country list input
-    $('label[for="llc_countries"]').attr('for', 'llc_countries_js');
+    $("label[for='llc_countries']").attr("for", "llc_countries_js");
 
     // hide elements with .no-js class when javascript is available
-    $('#llc-options-page .no-js').hide();
+    $("#llc-options-page .no-js").hide();
 
     // we initialize the new country list input
     // TODO: Do this on element creation
-    $('#llc_countries_js')
+    $("#llc_countries_js")
         .textext({
-            plugins: 'autocomplete tags',
+            plugins: "autocomplete tags",
             tags: {
                 // we preset the list with the current value from no-js text input
-                items: $('#llc_countries').val().split(',')
+                items: $("#llc_countries").val().split(",")
             }
         })
-        .bind('getSuggestions', function (e, data) {
+        .bind("getSuggestions", function (e, data) {
             var list = llc_country_codes,
                 textext = $(e.target).textext()[0],
-                query = (data ? data.query : '') || ''
+                query = (data ? data.query : "") || ""
                 ;
 
             $(this).trigger(
-                'setSuggestions',
+                "setSuggestions",
                 {result: textext.itemManager().filter(list, query)}
             );
         })
         // we write all changes in our new input to the old one for submission
-        .bind('setFormData', function (e, data, isEmpty) {
+        .bind("setFormData", function (e) {
             var textext = $(e.target).textext()[0];
             textext = textext.hiddenInput().val();
-            $('#llc_countries').val(textext);
+            $("#llc_countries").val(textext);
         });
 });
