56f92644ded827cb541ac0beac9ff1a16c755a09
wedi <code@dirk-weise.de>
2014-10-14 07:59:25 +0200

    Remove Author data and URLs from translation strings.

--- Gruntfile.js
+++ Gruntfile.js
@@ -130,6 +130,21 @@ module.exports = function( grunt ) {
                     'last-translator': 'WP-Translators (http://WP-Translators.org/)'
                 },
                 processPot: function( pot ) {
+                    var translation,
+                        excludedMeta = [
+                            'Plugin Name of the plugin/theme',
+                            'Plugin URI of the plugin/theme',
+                            'Author of the plugin/theme',
+                            'Author URI of the plugin/theme'
+                        ];
+                    for ( translation in pot.translations[''] ) {
+                        if ( 'undefined' !== typeof pot.translations[ '' ][ translation ].comments.extracted ) {
+                            if ( excludedMeta.indexOf( pot.translations[ '' ][ translation ].comments.extracted) >= 0 ) {
+                                console.log( 'Excluded meta: ' + pot.translations[ '' ][ translation ].comments.extracted );
+                                delete pot.translations[ '' ][ translation ];
+                            }
+                        }
+                    }
                     delete pot.headers['x-generator'];
                     return pot;
                 },
